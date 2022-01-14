2233e8326ef1cfad4533f38d1fc663865cad249b
Matt Burke <spraints@gmail.com>
2022-01-13 22:13:47 -0500

    make it a color png
    I want to be able to draw things on the image as I learn about OCR.

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,34 @@
-use image::{GenericImageView, Pixel};
+use image::{io::Reader, DynamicImage, GenericImageView, ImageBuffer, Pixel, Rgb};
 use std::env;
 use std::error::Error;
-use std::fs::File;
 
 fn main() {
     let mut args = env::args().skip(1);
+    let cutoff = 115; // todo make this a command line arg. Lower number makes more white.
     match args.next() {
         None => println!("Usage: boggle-ocr IMAGE"),
-        Some(arg) => dump(&arg).unwrap(),
+        Some(arg) => dump(&arg, cutoff).unwrap(),
     };
 }
 
-fn dump(path: &str) -> Result<(), Box<dyn Error>> {
-    let img = image::io::Reader::open(path)?.decode()?;
-    let encoder = image::codecs::png::PngEncoder::new(File::create("grayscale.png")?);
-    let (x, y) = img.dimensions();
-    let cutoff = 115; // todo make this a command line arg. Lower number makes more white.
-    encoder.encode(&lumas(&img, cutoff), x, y, image::ColorType::L8)?;
+fn dump(path: &str, cutoff: u8) -> Result<(), Box<dyn Error>> {
+    let img = Reader::open(path)?.decode()?;
+    let res = make_bw_rbg(img, cutoff);
+    res.save("computer-vision.png")?;
     Ok(())
 }
 
-fn lumas(img: &image::DynamicImage, cutoff: u8) -> Vec<u8> {
-    img.pixels().map(|(_, _, p)| bw(p, cutoff)).collect()
-}
-
-fn bw<P: Pixel<Subpixel = u8>>(p: P, cutoff: u8) -> u8 {
-    let v = p.to_luma().0[0];
-    if v > cutoff {
-        255
-    } else {
-        0
+fn make_bw_rbg(img: DynamicImage, cutoff: u8) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+    let (w, h) = img.dimensions();
+    let mut res = ImageBuffer::new(w, h);
+    for (x, y, p) in img.pixels() {
+        let mut bw = p.to_luma();
+        if bw.0[0] > cutoff {
+            bw.0[0] = 255;
+        } else {
+            bw.0[0] = 0;
+        }
+        res.put_pixel(x, y, bw.to_rgb());
     }
+    res
 }

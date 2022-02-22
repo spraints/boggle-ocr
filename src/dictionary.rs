bc3d9d64c3cdb52576f3aeab12e2f2f87b75c0a5
Matt Burke <spraints@gmail.com>
2022-02-21 19:37:14 -0500

    try to do this

--- src/dictionary.rs
+++ src/dictionary.rs
@@ -1,15 +1,17 @@
 use serde::de::{Deserializer, MapAccess, Visitor};
+use std::cell::RefCell;
 use std::collections::HashMap;
 use std::error::Error;
 use std::fs::read_to_string;
 use std::rc::Rc;
 
 // DAWG based on https://jbp.dev/blog/dawg-basics.html
+// and https://github.com/sile/rust-dawg
 
 pub fn open() -> Result<Dictionary, Box<dyn Error>> {
     let j = read_to_string("OWL2.json")?;
     let mut de = serde_json::Deserializer::from_str(&j);
-    let mut builder = DictBuilder::new();
+    let mut builder = DictionaryBuilder::new();
     let mut n = 0;
     for (word, _) in de.deserialize_map(OWLVisitor::new())? {
         builder.insert(word, n < 10);
@@ -18,18 +20,20 @@ pub fn open() -> Result<Dictionary, Box<dyn Error>> {
     Ok(builder.into_dict())
 }
 
-struct DictBuilder {
+type RcNodeBuilder = Rc<RefCell<NodeBuilder>>;
+
+struct DictionaryBuilder {
     previous_word: Option<String>,
-    root: Rc<Node>,
-    minimized: HashMap<Rc<Node>, Rc<Node>>,
-    unchecked_nodes: Vec<(Rc<Node>, char, Rc<Node>)>,
+    root: RcNodeBuilder,
+    minimized_nodes: HashMap<RcNodeBuilder, RcNodeBuilder>,
+    unchecked_nodes: Vec<(RcNodeBuilder, char, RcNodeBuilder)>,
 }
 
-impl DictBuilder {
+impl DictionaryBuilder {
     fn new() -> Self {
         Self {
             previous_word: None,
-            root: Rc::new(Node::new()),
+            root: RcNodeBuilder::new(),
             minimized: HashMap::new(),
             unchecked_nodes: vec![],
         }
@@ -46,15 +50,26 @@ impl DictBuilder {
         }
         self.minimize(common_prefix);
 
-        let suffix = Node::for_suffix(word.chars().skip(common_prefix), debug);
-        if debug {
-            println!("todo: add suffix {} to the right thing", suffix.unwrap().id);
+        let mut node = match self.unchecked_nodes.last() {
+            None => self.root.borrow_mut(),
+            Some((_, _, x)) => x.borrow_mut(),
+        };
+
+        for letter in word.chars().skip(common_prefix) {
+            let next_node = RcNodeBuilder::new();
+            node.set_child(letter, next_node);
+            self.unchecked_nodes.push((node, letter, next_node));
+            node = next_node;
         }
+
+        node.terminal = false;
         self.previous_word = Some(word);
     }
 
     fn into_dict(self) -> Dictionary {
-        Dictionary { root: self.root }
+        Dictionary {
+            root: self.root.into_node(0),
+        }
     }
 
     fn common_prefix(&self, word: &str) -> usize {
@@ -74,8 +89,61 @@ impl DictBuilder {
         }
     }
 
-    fn minimize(&mut self, common_prefix: usize) {
-        // todo
+    fn minimize(&mut self, down_to: usize) {
+        while self.unchecked_nodes.len() > down_to {
+            let (parent, letter, child) = self.unchecked_nodes.pop().unwrap();
+            match self.minimized_nodes.get(child) {
+                Some(new_child) => parent.children.borrow_mut().set_child(letter, new_child),
+                None => self.minimized_nodes.insert(child, child),
+            };
+        }
+    }
+}
+
+struct NodeBuilder {
+    terminal: bool,
+    children: Vec<Option<RcNodeBuilder>>,
+}
+
+impl NodeBuilder {
+    fn new() -> Self {
+        Self {
+            terminal: false,
+            children: vec![None; 26],
+        }
+    }
+
+    fn set_child(&mut self, letter: char, child: RcNodeBuilder) {
+        panic!("todo");
+    }
+
+    fn into_node(self) -> Node {
+        let (node, _) = self.finish(0);
+        node
+    }
+
+    fn finish(self, mut n: usize) -> (Node, usize) {
+        let id = n;
+        let NodeBuilder { terminal, children } = self;
+        let children = children
+            .into_iter()
+            .map(|c| match c {
+                None => None,
+                Some(c) => {
+                    let (a, b) = c.into_inner().finish(n);
+                    n = b;
+                    Some(a)
+                }
+            })
+            .collect();
+        (
+            Node {
+                terminal,
+                id,
+                children,
+            },
+            n,
+        )
     }
 }
 
@@ -90,49 +158,47 @@ struct Node {
     children: Vec<Option<Node>>,
 }
 
-const TODO_ID: usize = 0;
-
-impl Node {
-    fn new() -> Self {
-        Self {
-            terminal: false,
-            id: 0,
-            children: vec![None; 26],
-        }
-    }
-
-    fn for_suffix<C: Iterator<Item = char>>(mut chars: C, debug: bool) -> Option<Self> {
-        match chars.next() {
-            None => None,
-            Some(c) => {
-                let mut res = Self::new();
-                match Self::for_suffix(chars, debug) {
-                    None => {
-                        if debug {
-                            println!("  suffix {}: terminal", c);
-                        }
-                        res.terminal = true
-                    }
-                    Some(n) => {
-                        if debug {
-                            println!("  suffix: {}: has more suffix", c);
-                        }
-                        res.set(c, n)
-                    }
-                };
-                Some(res)
-            }
-        }
-    }
-
-    fn set(&mut self, c: char, child: Self) {
-        let pos = c.to_lowercase().next().unwrap() as u8 - b'a';
-        assert!(pos >= 0 && pos < 26);
-        let pos = pos as usize;
-        assert!(self.children[pos].is_none());
-        self.children[pos] = Some(child);
-    }
-}
+// impl Node {
+//     fn new() -> Self {
+//         Self {
+//             terminal: false,
+//             id: 0,
+//             children: vec![None; 26],
+//         }
+//     }
+//
+//     fn for_suffix<C: Iterator<Item = char>>(mut chars: C, debug: bool) -> Option<Self> {
+//         match chars.next() {
+//             None => None,
+//             Some(c) => {
+//                 let mut res = Self::new();
+//                 match Self::for_suffix(chars, debug) {
+//                     None => {
+//                         if debug {
+//                             println!("  suffix {}: terminal", c);
+//                         }
+//                         res.terminal = true
+//                     }
+//                     Some(n) => {
+//                         if debug {
+//                             println!("  suffix: {}: has more suffix", c);
+//                         }
+//                         res.set(c, n)
+//                     }
+//                 };
+//                 Some(res)
+//             }
+//         }
+//     }
+//
+//     fn set(&mut self, c: char, child: Self) {
+//         let pos = c.to_lowercase().next().unwrap() as u8 - b'a';
+//         assert!(pos >= 0 && pos < 26);
+//         let pos = pos as usize;
+//         assert!(self.children[pos].is_none());
+//         self.children[pos] = Some(child);
+//     }
+// }
 
 struct OWLVisitor {}
 

2c360f571333d547d7bbc300fdaccb3fbac298ca
Matt Burke <spraints@gmail.com>
2022-07-12 07:25:45 -0400

    tell me which letters to use

--- wordle-cheat/app/controllers/the_cheat_controller.rb
+++ wordle-cheat/app/controllers/the_cheat_controller.rb
@@ -9,6 +9,21 @@ class TheCheatController < ApplicationController
     if guess = params[:guess]
       @guesses << Guess.new(guess)
     end
-    @remaining_words = Guesstimator.guess(@guesses)
+    unless @guesses.empty?
+      @remaining_words = Guesstimator.guess(@guesses)
+      @letter_counts = count_letters(@remaining_words)
+    end
+  end
+
+  private
+
+  def count_letters(words)
+    counts = Hash.new(0)
+    words.each do |w|
+      w.chars.uniq.each do |c|
+        counts[c] += 1
+      end
+    end
+    counts
   end
 end

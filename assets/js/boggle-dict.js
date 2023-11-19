2a1c46ca1f84582db7c72529d129241e334fc054
Matt Burke <spraints@gmail.com>
2023-11-18 20:15:20 -0500

    Make the dictionary look better

--- assets/js/boggle-dict.js
+++ assets/js/boggle-dict.js
@@ -1,21 +1,45 @@
 const word = document.querySelector("#word")
-const result = document.querySelector("#result")
+const results = document.querySelectorAll(".result")
+const resultFound = document.querySelector(".dictionary-entry")
+const foundWord = document.querySelector(".dictionary-word")
+const foundDefinition = document.querySelector(".dictionary-definition")
+const resultError = document.querySelector(".not-found")
 
 word.addEventListener("input", async function() {
   const s = word.value
+  for (e of results) {
+    e.classList.add("result-hidden")
+  }
   if (s == "") {
-    result.innerText = ""
     return
   }
-  const resp = await fetch(`/boggle/dict/words/${encodeURIComponent(s)}`)
-  const res = await resp.text()
-  if (s == word.value) {
-    if (resp.status == 200) {
-      result.innerText = `definition of ${s}: ${res}`
-    } else if (resp.status == 404) {
-      result.innerText = `${s}: not a word`
-    } else {
-      result.innerText = `HTTP ${resp.status} ${res}`
+
+  try {
+    const resp = await fetch(`/boggle/dict/words/${encodeURIComponent(s)}`)
+    const res = await resp.text()
+    if (s == word.value) {
+      for (e of results) {
+        e.classList.add("result-hidden")
+      }
+      if (resp.status == 200) {
+        foundWord.innerText = s
+        foundDefinition.innerText = res
+        resultFound.classList.remove("result-hidden")
+      } else if (resp.status == 404) {
+        resultError.innerText = `${s}: not a word`
+        resultError.classList.remove("result-hidden")
+      } else {
+        resultError.innerText = `HTTP ${resp.status} ${res}`
+        resultError.classList.remove("result-hidden")
+      }
+    }
+  } catch {
+    if (s == word.value) {
+      for (e of results) {
+        e.classList.add("result-hidden")
+      }
+      resultError.innerText = "server unavailable"
+      resultError.classList.remove("result-hidden")
     }
   }
 })
